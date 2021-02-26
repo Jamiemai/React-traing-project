a8c4942b45895e5a946ee7011f7a8ca00a35836b
Jamie <maicuong1701@gmail.com>
2021-02-26 14:49:00 +0700

    Initial commit

---
+++ jamazon/jamazon/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC0v_5nAS6dyadSRoZomqeXGd_4FB6y4d0",
+    authDomain: "j-51b17.firebaseapp.com",
+    projectId: "j-51b17",
+    storageBucket: "j-51b17.appspot.com",
+    messagingSenderId: "34946139587",
+    appId: "1:34946139587:web:89b5393c5255c3ebef6976",
+    measurementId: "G-X66NG9CKRG"
+  };
